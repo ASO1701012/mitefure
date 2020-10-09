42df1e7b685e42c84fb46e90bfb61f4f8f6db46a
1801187 <1801187@s.asojuku.ac.jp>
2020-10-09 15:01:24 +0900

    画像表示完了

--- data/www/realfriend/src/components/SnsShareButton.vue
+++ data/www/realfriend/src/components/SnsShareButton.vue
@@ -1,18 +1,35 @@
 <template>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button"
-       data-show-count="false">Tweet</a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    <a href="#" class="instagram">
-      <img src="/static/instagram_icon.svg" alt="読み込み失敗">
+  <div class="parent">
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" >
+      <img src="../../static/twitter_icon.svg" alt="">
     </a>
+    <a href="#" class="instagram">
+      <img src="../../static/instagram_icon.svg" alt="読み込み失敗">
+    </a>
+  </div>
+
 </template>
 
 <script>
 export default {
 name: "SnsShareButton"
 }
+twitter:{
+
+}
 </script>
 
 <style scoped>
+  /*.parent{*/
+  /*  display: flex;*/
+  /*  width: 80%;*/
+  /*}*/
 
+  /*.parent > a{*/
+  /*  flex-direction: row;*/
+  /*}*/
+
+  /*.parent > img {*/
+  /*  width: 40%;*/
+  /*}*/
 </style>
