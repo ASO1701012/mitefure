c14b0ad2afc328c02bad6ccf699ce1c1d386e68d
aso1701011 <1701011@st.asojuku.ac.jp>
2020-10-09 12:16:15 +0900

    Titile画面にロゴとタイトルが表示されるcomponentを作成しました。

---
+++ data/www/realfriend/src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img v-bind:src="logoImageSrc">
+    <h2>REAL FRIEND</h2>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Title",
+    data() {
+      return {
+        logoImageSrc: require("@/assets/realogo.png"),
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
