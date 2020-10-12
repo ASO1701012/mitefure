27042d91fb3200a8eb28c5a01b384de54a29f07b
TakahiroMatsuo <1801187@s.asojuku.ac.jp>
2020-10-12 11:55:33 +0900

    ガワ完成できればボタンそれぞれを分割したい

--- data/www/realfriend/src/components/SnsShareButton.vue
+++ data/www/realfriend/src/components/SnsShareButton.vue
@@ -1,35 +1,46 @@
 <template>
   <div class="parent">
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" >
-      <img src="../../static/twitter_icon.svg" alt="">
-    </a>
-    <a href="#" class="instagram">
-      <img src="../../static/instagram_icon.svg" alt="読み込み失敗">
-    </a>
+    <div class="icon_list">
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">
+        <img src="../../static/twitter_icon.svg" alt="">
+      </a>
+    </div>
+    <div class="icon_list">
+      <a href="#">
+        <img src="../../static/discord_icon.svg" alt="読み込み失敗">
+      </a>
+    </div>
+    <div class="icon_list">
+      <a href="#">
+        <img src="../../static/instagram_icon.svg" alt="読み込み失敗">
+      </a>
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
-name: "SnsShareButton"
+  name: "SnsShareButton"
 }
-twitter:{
 
-}
 </script>
 
 <style scoped>
-  /*.parent{*/
-  /*  display: flex;*/
-  /*  width: 80%;*/
-  /*}*/
+.parent {
+  display: flex;
+  justify-content: space-around;
+  width: 20%;
+}
 
-  /*.parent > a{*/
-  /*  flex-direction: row;*/
-  /*}*/
+.icon_list {
+  width: 30vh;
+}
 
-  /*.parent > img {*/
-  /*  width: 40%;*/
-  /*}*/
+/*!*.parent > a{*!*/
+/*!*  flex-direction: row;*!*/
+/*!*}*!*/
+
+/*.parent > img {*/
+/*  width: 40%;*/
+/*}*/
 </style>
