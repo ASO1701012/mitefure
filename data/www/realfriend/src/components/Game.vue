7ad96c2b04b99790dd6828a99c848803fdd5a639
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-10-23 14:09:32 +0900

    creare Game.vue GameCamera.vue GameStatus.vue

---
+++ data/www/realfriend/src/components/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <GameCamera></GameCamera>
+    <GameStatus></GameStatus>
+  </div>
+</template>
+
+<script>
+    import GameCamera from "./GameCamera"
+    import GameStatus from "./GameStatus"
+
+    export default {
+        name: "Game.vue",
+        components: {GameStatus, GameCamera}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ data/www/realfriend/src/components/GameCamera.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <video ref="video" id="video" width="500" height="500" autoplay muted></video>
+    <!--    canvasを表示しないようにする-->
+    <canvas ref="canvas" id="canvas" hidden></canvas>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "GameCamera",
+        data() {
+            return {
+                image: null, //顔データを保存する
+                video: {},   //streamを保持させる
+                canvas: {},  //canvas領域
+
+                postUrl: 'https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/emotion',
+            }
+        },
+        methods: {
+            faceApi() {
+                //faceApiに顔データを送信
+                this.axios.post(this.postUrl, {
+                    image: String(this.image),
+                },).then(function (response) {
+                    console.log(response.status)
+                    if (response.status == 200) {
+                        console.log(response)
+                    } else {
+                        console.log(response)
+                    }
+                }).catch(function (error) {
+                    console.log(error)
+                })
+            },
+            capture(){
+
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
