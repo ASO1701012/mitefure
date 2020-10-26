6fe0e7c2cc861d217003080d996bfab21295c569
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-10-26 12:58:39 +0900

    ３秒後にカメラを起動するように設定

--- data/www/realfriend/src/components/GameCamera.vue
+++ data/www/realfriend/src/components/GameCamera.vue
@@ -2,7 +2,7 @@
   <div>
     <video ref="video" id="video" width="500" height="500" autoplay muted></video>
     <!--    canvasを表示しないようにする-->
-    <canvas ref="canvas" id="canvas" hidden></canvas>
+    <canvas ref="canvas" id="canvas" width="500" height="500" hidden></canvas>
     <button v-on:click="videoStart">カメラ起動</button>
   </div>
 </template>
@@ -15,9 +15,14 @@
                 image: null, //顔データを保存する
                 video: {},   //streamを保持させる
                 canvas: {},  //canvas領域
+                timer: null, //インターバル用のタイマー
 
                 postUrl: 'https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/emotion',
             }
+        },
+        mounted() {
+            //画面が開いてから３秒後にカメラを起動
+            setTimeout(this.videoStart, 3000)
         },
         methods: {
             faceApi() {
@@ -28,6 +33,7 @@
                     console.log(response.status)
                     if (response.status == 200) {
                         console.log(response)
+                        console.log(response.data.anger+"アンガー")
                     } else {
                         console.log(response)
                     }
@@ -36,12 +42,14 @@
                 })
             },
             capture(){
+                console.log("captureに入りました")
                 //カメラが写っている範囲を指定し、その領域を画像として切り取る
                 this.canvas = this.$refs.canvas
                 this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480)
                 //画像データをbase64にエンコード
                 this.image = this.canvas.toDataURL("image/jpeg")
                 this.image = this.image.substr(23)
+                this.faceApi()
             },
             videoStart() {
                 this.video = this.$refs.video
@@ -49,10 +57,26 @@
                     navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
                         this.video.srcObject = stream
                         this.video.play()
+
+
+                        this.timer =setInterval(this.capture, 3000)
+
+                        //14秒後に撮影を終了する
+                        setTimeout(this.captureStop, 14000)
+                        //20秒後にカメラを停止する
+                        setTimeout(this.videoStop, 20000)
                     })
                 } else {
                     console.log("getUserMedia not support")
                 }
+            },
+            captureStop() {
+                clearInterval(this.timer)
+                this.$router.push('/load')
+            },
+            videoStop() {
+                console.log("ストップ")
+                this.video.srcObject.getTracks().forEach(track => track.stop())
             }
         }
 
