5442b133e472655064431abc2a5aa28c08fe4b13
TakahiroMatsuo <1801187@s.asojuku.ac.jp>
2020-10-30 12:11:07 +0900

    OGPが稼働するようになった。Twitterのシェア機能を実装しているがエラーで難航している。

--- data/www/realfriend/src/components/TwitterShareButton.vue
+++ data/www/realfriend/src/components/TwitterShareButton.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw">
+<!--    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-text="aaaaaaaa">-->
+<!--      <img src="/static/twitter_icon.svg" alt="">-->
+<!--    </a>-->
+    <a :href="finalurl">
       <img src="/static/twitter_icon.svg" alt="">
     </a>
+<!--    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: "TwitterShareButton"
+  name: "TwitterShareButton",
+  data(){
+    return{
+      aaurl: "https://twitter.com/share?url=https://realfriend.online/&text=",
+      aatext: "aaaaaaa",
+      finalurl: this.aaurl + this.aatext,
+    }
+  },
+  created() {
+    console.log(this.finalurl,this.aaurl,this.aatext)
+  }
 }
 </script>
 
