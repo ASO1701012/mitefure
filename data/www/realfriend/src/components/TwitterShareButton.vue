9b5305c3ee9d62be55d761ca26842d3dbaef3ac3
TakahiroMatsuo <1801187@s.asojuku.ac.jp>
2020-10-30 12:31:55 +0900

    Twitterに文字列を共有することが可能になった。

--- data/www/realfriend/src/components/TwitterShareButton.vue
+++ data/www/realfriend/src/components/TwitterShareButton.vue
@@ -3,7 +3,7 @@
 <!--    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-text="aaaaaaaa">-->
 <!--      <img src="/static/twitter_icon.svg" alt="">-->
 <!--    </a>-->
-    <a :href="finalurl">
+    <a :href="url">
       <img src="/static/twitter_icon.svg" alt="">
     </a>
 <!--    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
@@ -15,13 +15,21 @@ export default {
   name: "TwitterShareButton",
   data(){
     return{
-      aaurl: "https://twitter.com/share?url=https://realfriend.online/&text=",
-      aatext: "aaaaaaa",
-      finalurl: this.aaurl + this.aatext,
+      url:"",
+      text: "aaa"
     }
   },
   created() {
     console.log(this.finalurl,this.aaurl,this.aatext)
+    let url = new URL("https://twitter.com/share")
+    url.searchParams.set("url","https://realfriend.online/")
+    url.searchParams.set("text",this.text)
+    this.url = url
+  },
+  methods: {
+    urlGenerate: function (){
+      this.$store.getters[]
+    }
   }
 }
 </script>
