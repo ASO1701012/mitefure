a7926d31d63f6f74d645d354704caddfb10b9e47
TakahiroMatsuo <1801187@s.asojuku.ac.jp>
2020-11-04 14:29:04 +0900

    不要なファイルを削除

--- data/www/realfriend/src/components/TwitterShareButton.vue
+++ data/www/realfriend/src/components/TwitterShareButton.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
-<!--    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-text="aaaaaaaa">-->
-<!--      <img src="/static/twitter_icon.svg" alt="">-->
-<!--    </a>-->
-    <a :href="url">
+   <a :href="url">
       <img src="/static/twitter_icon.svg" alt="">
     </a>
-<!--    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>-->
   </div>
 </template>
 
@@ -19,10 +15,9 @@ export default {
     }
   },
   created() {
-    console.log(this.finalurl,this.aaurl,this.aatext)
     let url = new URL("https://twitter.com/share")
     url.searchParams.set("url","https://realfriend.online/")
-    url.searchParams.set("text",this.$store.getters['Favo/getEmotionText'])
+    url.searchParams.set("text",this.$store.getters['Favo/getResultText'])
     this.url = url
   },
 }
