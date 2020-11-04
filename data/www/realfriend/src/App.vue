111e0122dc461e1803a8cec91de9bd7a01a47208
Ryuana <huziie1999@icloud.com>
2020-11-04 10:23:00 +0900

    Merge branch 'develop' of /Users/ryu/Documents/realfriend_source with conflicts.

--- data/www/realfriend/src/App.vue
+++ data/www/realfriend/src/App.vue
@@ -8,52 +8,17 @@
 
 <script>
 
-export default {
-  name: 'App',
-  // beforeCreate() {
-  //     //vueインスタンス生成時
-  //     this.$store.dispatch("token/localStorageLoad")
-  // },
-  // created() {
-  //     window.addEventListener("beforeunload", this.confirmSave)
-  // },
-  // destroyed() {
-  //     window.removeEventListener("beforeunload", this.confirmSave)
-  //     // thislog(http.request.headers.Authorization)
-  // },
-  methods: {
-
-
-    // confirmSave(event) {
-    //     this.counterSave()
-    //     event.returnValue = "編集中のものは保存されませんが、よろしいですか？"
-    // },
-    // counterSave() {
-    //     let me = this
-    //     //ユーザー動向のカウンター値をDBに保存する
-    //     this.axios.post('https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/counter', {
-    //         login_count: this.$store.state.counter.loginCount,
-    //         autoLogin_count: this.$store.state.counter.autoLoginCount,
-    //         camera_count: this.$store.state.counter.cameraCount,
-    //         news_count: this.$store.state.counter.newsCount
-    //     }).then(function (response) {
-    //         me.$store.dispatch("resetCounter")
-    //         console.log(response)
-    //     }).catch(function (error) {
-    //         console.log(error)
-    //     })
-    // }
-  }
-}
+    export default {
+        name: 'App',
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-image: url("assets/log.png");
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
