0764fce81eaf2117374f9cec8f7634016705e436
Ryuana <huziie1999@icloud.com>
2020-11-17 14:18:47 +0900

    Create 診断エラー時のメッセージ
    診断で、全てエラーが出た際に、ハートや診断メッセージなどを非表示にする仕組みを作成しました。
    また、エラー時のメッセージを表示するよう作成しました。

--- data/www/realfriend/src/components/Result.vue
+++ data/www/realfriend/src/components/Result.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <ReturnButton class="return-position"/>
-    <ResultRatioDisplay class="ratio-position" :point="point"/>
-    <ResultHeart class="heart-position" :point="point"/>
-    <ResultTextArea class="text-position" :result-text="resultText"/>
-    <ShareButtons class="button-position"/>
+    <ReturnButton v-if="!error" class="return-position"/>
+    <ResultRatioDisplay v-if="!error" class="ratio-position" :point="point"/>
+    <ResultHeart v-if="!error" class="heart-position" :point="point"/>
+    <ResultTextArea v-if="!error" class="text-position" :result-text="resultText"/>
+    <ShareButtons v-if="!error" class="button-position"/>
+    <div v-if="error" class="error">
+      うまく顔を認識出来ませんでした。<br>
+      顔を近づける、または、少し離すことで改善されるかも。
+    </div>
   </div>
 </template>
 
@@ -21,63 +25,82 @@
         data() {
             return {
                 point: 0,
-                resultText: ""
+                resultText: "",
+                error: false
             }
         },
         created() {
             this.point = this.$store.getters['Favo/getEmotionPoint']
             this.resultText = this.$store.getters['Favo/getResultText']
+            this.checkError(this.point)
             window.addEventListener("beforeunload", this.confirmSave)
         },
         destroyed() {
             window.removeEventListener("beforeunload", this.confirmSave)
         },
         beforeRouteLeave(to, from, next) {
-            if (to.name === "Title"){
+            if (to.name === "Title") {
                 next()
-            }else{
-              next(false)
+            } else {
+                next(false)
             }
 
         },
         methods: {
             confirmSave(event) {
                 event.returnValue = "本当に遷移してもよろしいですか？"
+            },
+            checkError: function (point) {
+                if (point === -1) {
+                    this.error = true
+                }
             }
         },
     }
 </script>
 
 <style scoped>
-@media screen and (max-width: 480px) {
-  .button-position{
-    position: absolute;
-    bottom: 5%;
-    right: 5%;
+  @media screen and (max-width: 480px) {
+    .button-position {
+      position: absolute;
+      bottom: 5%;
+      right: 5%;
+    }
   }
-}
-/*スマホサイズでない場合に反映される*/
-@media screen and (min-width: 481px) {
-  .button-position{
-    position: absolute;
-    bottom: 5%;
-    right: 5%;
-  }
-}
-.return-position{
-  float:left;
-  position: absolute;
-}
-.ratio-position{
-  font-size: x-large;
-}
-.heart-position{
 
-}
-.text-position{
-  margin: auto;padding-top: 5%;line-height: 2em;
-}
-/*.button-position{*/
-/*  float: right;*/
-/*}*/
+  /*スマホサイズでない場合に反映される*/
+  @media screen and (min-width: 481px) {
+    .button-position {
+      position: absolute;
+      bottom: 5%;
+      right: 5%;
+    }
+  }
+
+  .return-position {
+    float: left;
+    position: absolute;
+  }
+
+  .ratio-position {
+    font-size: x-large;
+  }
+
+  .heart-position {
+
+  }
+
+  .text-position {
+    margin: auto;
+    padding-top: 5%;
+    line-height: 2em;
+  }
+
+  .error {
+    margin-top: 20%;
+  }
+
+  /*.button-position{*/
+  /*  float: right;*/
+  /*}*/
 </style>
