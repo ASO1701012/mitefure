c4cc0fb8d4fba786e8516c83e7ae8f575f96864b
Ryuana <huziie1999@icloud.com>
2020-11-17 16:24:28 +0900

    Update Title
    タイトルの説明文を改良しました

--- data/www/realfriend/src/components/Title.vue
+++ data/www/realfriend/src/components/Title.vue
@@ -5,12 +5,12 @@
     <div class="container mx-auto container-style">
       <div class="justify-content-center input-style">
 
-      <NormalTextBox
-        v-model="getTextBoxText"
-        placeholder="相手の名前を入れてね"
-        name="textBoxInput"
-        type="text"
-        class="text-style border border-dark"      ></NormalTextBox>
+        <NormalTextBox
+          v-model="getTextBoxText"
+          placeholder="相手の名前を入れてね"
+          name="textBoxInput"
+          type="text"
+          class="text-style border border-dark"></NormalTextBox>
 
         <NormalButton class="buttonInput btn btn-outline-secondary" v-on:click="start">診断</NormalButton>
 
@@ -18,13 +18,15 @@
     </div>
 
 
-      <NormalTextArea class="text-wrap text-area-style">
-        説明文：このアプリは相手の表情もとにその人の感情を調べ、
-        あなたと話しているときの反応を調べ好感度を表示するアプリです。
-        友達や気になるあの人を気軽に診断してみよう!
-      </NormalTextArea>
+    <NormalTextArea class="text-wrap text-area-style">
+      説明文：診断アプリ【RealFriend】は相手の表情から感情を読み取る事が出来ます。<br>
+      名前入力後、診断を押すと3秒後にカメラが起動し、感情を読み取り始めます。<br>
+      15秒ほど読み取った後、感情を元に好感度をスコア化して表示します。<br>
+      友人や気になる人との会話で診断し、どんな感情なのか調べてみましょう！
+    </NormalTextArea>
     <div class="follow-button">
-      <a class="twitter-follow-button" href="https://twitter.com/AsoRealFriend" data-size="large" target="_blank" rel="noopener">
+      <a class="twitter-follow-button" href="https://twitter.com/AsoRealFriend" data-size="large" target="_blank"
+         rel="noopener">
         Follow @AsoRealFriend</a>
     </div>
   </div>
@@ -36,108 +38,115 @@
     import NormalButton from "./NormalButton"
 
     export default {
-    name: "Title",
-    components: {NormalTextBox: NormalTextBox, NormalTextArea: NormalTextArea, NormalButton: NormalButton},
-    data() {
-      return {
-        getTextBoxText: '',
-      }
-    },
-    methods: {
-        start() {
-            const EXECUTE = async () => {
-                const setFriendName = () => {
-                    return new Promise((resolve, reject) => {
-                        setTimeout(() => {
-                            this.$store.dispatch('Friend/nameSet', this.getTextBoxText)
-                            console.log("セットフレンドネーム")
-                            resolve()
-                        }, 100)
-                    })
-                }
-
-                const startGame = () => {
-                    return new Promise(((resolve, reject) => {
-                        setTimeout(() => {
-                            console.log("移動します")
-                            this.$router.push('/game')
-                            resolve()
-                        }, 100)
-                    }))
-                }
-
-                await setFriendName()
-                await startGame()
+        name: "Title",
+        components: {NormalTextBox: NormalTextBox, NormalTextArea: NormalTextArea, NormalButton: NormalButton},
+        data() {
+            return {
+                getTextBoxText: '',
             }
+        },
+        methods: {
+            start() {
+                const EXECUTE = async () => {
+                    const setFriendName = () => {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                this.$store.dispatch('Friend/nameSet', this.getTextBoxText)
+                                console.log("セットフレンドネーム")
+                                resolve()
+                            }, 100)
+                        })
+                    }
 
-          if(this.getTextBoxText.length <= 10){
-            if (this.getTextBoxText != '') {
-              EXECUTE()
+                    const startGame = () => {
+                        return new Promise(((resolve, reject) => {
+                            setTimeout(() => {
+                                console.log("移動します")
+                                this.$router.push('/game')
+                                resolve()
+                            }, 100)
+                        }))
+                    }
+
+                    await setFriendName()
+                    await startGame()
+                }
+
+                if (this.getTextBoxText.length <= 10) {
+                    if (this.getTextBoxText != '') {
+                        EXECUTE()
+                    } else {
+                        alert('名前を入力してください！')
+                    }
+                } else {
+                    alert('名前を10文字以内にしてください')
+                }
+
+
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (to.name === "Game") {
+                next()
             } else {
-              alert('名前を入力してください！')
+                next(false)
+                // let answer = window.confirm("このページから移動しますか？ 入力したデータは保存されません。")
+                // if (answer) {
+                //   next()
+                // } else {
+                //   next(false)
+                // }
             }
-          } else {
-            alert('名前を10文字以内にしてください')
-          }
-
-
-      }
-    },
-      beforeRouteLeave(to, from, next) {
-        if (to.name === "Game") {
-          next()
-        } else {
-          next(false)
-          // let answer = window.confirm("このページから移動しますか？ 入力したデータは保存されません。")
-          // if (answer) {
-          //   next()
-          // } else {
-          //   next(false)
-          // }
         }
-      }
-  }
+    }
 </script>
 
 <style scoped>
 
-.container-style{
-  width: 100%;padding-top: 5%
-}
-.text-area-style{
-  margin: auto;padding-top: 5%;line-height: 2em;
-}
-
-/*スマホサイズ時に反映される*/
-@media screen and (max-width: 480px) {
-  .text-wrap{
+  .container-style {
     width: 100%;
+    padding-top: 5%
   }
-  .text-style{
-    width: 50%;
-    display: inline-block;
+
+  .text-area-style {
+    margin: auto;
+    padding-top: 5%;
+    line-height: 2em;
   }
-}
-/*スマホサイズでない場合に反映される*/
-@media screen and (min-width: 481px) {
-  .text-wrap{
-    width: 50%;
+
+  /*スマホサイズ時に反映される*/
+  @media screen and (max-width: 480px) {
+    .text-wrap {
+      width: 100%;
+    }
+
+    .text-style {
+      width: 50%;
+      display: inline-block;
+    }
   }
-}
+
+  /*スマホサイズでない場合に反映される*/
+  @media screen and (min-width: 481px) {
+    .text-wrap {
+      width: 50%;
+    }
+  }
 
 
-.follow-button {
-  background-color: #00b0ff;
-  width: 30vh;
-  height: 4vh;
-  margin: auto;
-  position: relative;
-}
-.follow-button a{
-  position: absolute;
-  display: block;
-  width: 100%;
-  height: 100%;
-  color: white;
-}
+  .follow-button {
+    background-color: #00b0ff;
+    width: 30vh;
+    height: 4vh;
+    margin: auto;
+    position: relative;
+  }
+
+  .follow-button a {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    color: white;
+  }
 </style>
