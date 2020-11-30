ec52aa3d380bcc5e5da9c5de96621c2157c13260
TakahiroMatsuo <1801187@s.asojuku.ac.jp>
2020-11-30 14:24:07 +0900

    discriptionのヘッダーを作成

---
+++ data/www/realfriend/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ReturnButton/>
+    <span>ミテフレ</span>
+  </div>
+</template>
+
+<script>
+import ReturnButton from "@/components/ReturnButton";
+export default {
+name: "Header",
+  components: {ReturnButton}
+}
+</script>
+
+<style scoped>
+
+</style>
