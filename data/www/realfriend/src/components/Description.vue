1653aef19269b47ac1a354910de546369b40cbb1
aso1701011 <1701011@st.asojuku.ac.jp>
2020-12-01 12:11:16 +0900

    slotないのテキストを表示させMessageCardVueでスタイルを指定すれば動くと思う...。

--- data/www/realfriend/src/components/Description.vue
+++ data/www/realfriend/src/components/Description.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-
+    <MessageCard>
+      シナリオ・・・
+    </MessageCard>
+    <br>
+    <MessageCard>
+      ミッション・・・
+    </MessageCard>
   </div>
 </template>
 
 <script>
+  import MessageCard from "./MessageCard"
+
   export default {
-    name: "Description"
+    name: "Description",
+    components: {MessageCard:MessageCard}
   }
 </script>
 
