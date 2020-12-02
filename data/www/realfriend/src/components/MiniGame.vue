f7798899330d4c437077adbf9be3d80648956e96
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-12-02 09:55:23 +0900

    MiniGame.vueの作成

--- data/www/realfriend/src/components/MiniGame.vue
+++ data/www/realfriend/src/components/MiniGame.vue
@@ -1,13 +1,41 @@
 <template>
-  <div></div>
+  <div class="game-style" ref="game">
+    <MiniGameCamera ref="camera" class="camera-style"></MiniGameCamera>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "MiniGame"
+    import MiniGameCamera from "@/components/MiniGameCamera"
+
+    export default {
+        name: "MiniGame",
+        components: {MiniGameCamera},
+        mounted() {
+            setTimeout(this.$refs.camera.videoStart, 3000)
+            this.$refs.status.countDownTimer()
+            let vh = window.innerHeight
+            this.$refs.game.style.height = vh + 'px'
+        },
+
   }
 </script>
 
 <style scoped>
+  .camera-style {
+    z-index: 1;
+  }
 
+  .status-style {
+    width: 80%;
+    position: absolute;
+    z-index: 2;
+    bottom: 1%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .game-style {
+    background-color: #000000;
+    height: 100vh;
+  }
 </style>
