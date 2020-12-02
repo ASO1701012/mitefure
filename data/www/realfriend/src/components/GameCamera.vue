2d504631526da436829b7029a37fe9cc9b9fd34a
Ryuana <huziie1999@icloud.com>
2020-12-02 14:22:55 +0900

    Revert "Create ミニゲーム文章の作成＆取り出し"
    This reverts commit ce427d3b

--- data/www/realfriend/src/components/GameCamera.vue
+++ data/www/realfriend/src/components/GameCamera.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="wrapper">
-    <video ref="video" width="100" height="100" playsinline="true" autoplay muted></video>
+    <video ref="video" width="0" height="0" playsinline="true" autoplay muted></video>
     <!--    canvasを表示しないようにする-->
     <canvas ref="canvasEffect" width="100" height="100"></canvas>
     </div>
@@ -99,20 +99,18 @@
                theCanvas.setAttribute('width',windowInnerWidth)
                theCanvas.setAttribute('height',windowInnerHeight)
 
-             this.c1 = this.$refs.video
-             theCanvas = this.c1
-             windowInnerWidth=window.innerWidth
-             windowInnerHeight=window.innerHeight
-             theCanvas.setAttribute('width',windowInnerWidth)
-             theCanvas.setAttribute('height',windowInnerHeight)
-
              theCanvas = image_id
              windowInnerWidth=window.innerWidth
              windowInnerHeight=window.innerHeight
              theCanvas.getContext("2d").drawImage(video_id, 0, 0 ,windowInnerWidth, windowInnerHeight)
                },
           videoStart() {
-                this.video = this.$refs.video
+            this.video = this.$refs.video
+            let theCanvas = this.video
+            let windowInnerWidth=window.innerWidth
+            let windowInnerHeight=window.innerHeight
+            theCanvas.setAttribute('width',windowInnerWidth)
+            theCanvas.setAttribute('height',windowInnerHeight)
               if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                   navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}}).then(stream => {
                       this.video.srcObject = stream
